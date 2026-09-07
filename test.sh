3a7003666eb08197ab476db656b7daa071735d5d
adhoo <andrew.hoo@aassert.com>
2026-09-06 22:01:50 -0400

    chore: appease github action

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p test-report
 go test -coverprofile=test-report/coverage.out -covermode=atomic -json ./... > test-report/test.json
-cat test-report/test.json | go-test-report -o test-report/test_report.html
+cat test-report/test.json | go tool go-test-report -o test-report/test_report.html
 go tool cover -html=test-report/coverage.out -o test-report/coverage.html
